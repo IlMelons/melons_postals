a768a1e53699d576b85b223918f2a5c86e50dd25
IlMelons <melonss2002@gmail.com>
2025-03-22 19:41:20 +0100

    Added all locales of servers using the script

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ lua54 "yes"
 name "melons_postals"
 author "IlMelons"
 description "Script that allows players to set a waypoint to specific postal codes with a simple command"
-version "1.1.1"
+version "1.2.0"
 repository "https://github.com/IlMelons/melons_postals"
 
 ox_lib "locale"
